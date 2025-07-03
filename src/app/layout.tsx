b9c0b8005c621c89c8142e65c7f24714ee6c5b20
deer-jay <h117d416@gmail.com>
2025-07-03 18:39:16 +0800

    配置eslint检查，添加静态资源

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} font-poppins antialiased`}
       >
         {children}
       </body>
