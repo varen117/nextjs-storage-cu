6266b93ce838fc2fcbf802c1e3c84ccfa3ae4a46
deer-jay <h117d416@gmail.com>
2025-07-06 09:57:48 +0800

    修复配置错误

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,6 +1,6 @@
 export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
-  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
   filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
